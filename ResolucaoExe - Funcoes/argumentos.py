df490bcbcf4ab542481126f4bf05c9e92cf9fe67
camilagomo <camilagomo@hotmail.com>
2024-07-23 15:09:01 -0300

    Resolucao exercicios argumentos 1

--- ResolucaoExe - Funcoes/argumentos.py
+++ ResolucaoExe - Funcoes/argumentos.py
@@ -3,3 +3,14 @@
 argumentos, e que forneça a soma desses três argumentos.
 
 '''
+
+def soma_tres_numeros(a, b, c):
+    return a + b + c
+
+# Exemplo de uso da função
+num1 = 5
+num2 = 10
+num3 = 15
+resultado = soma_tres_numeros(num1, num2, num3)
+
+print(f"A soma de {num1}, {num2} e {num3} é {resultado}")
